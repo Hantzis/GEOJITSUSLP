e623b2a3d72a43899936d9a637d070db18c26ade
Nizhi Saeba <mathereall@gmail.com>
2020-01-30 12:00:53 -0600

    m

--- GEOJITSUSLP/urls.py
+++ GEOJITSUSLP/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -15,6 +16,8 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
+
+    path('api/v1/', include('webgis.urls')),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
