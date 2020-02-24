7966a36645c59d3c01d0fce03f334da7e1fbd58f
mathereall@gmail.com <LifeStr34m>
2020-02-24 14:34:40 -0600

    inicial

--- GEOJITSUSLP/maptitudebox/src/App.vue
+++ GEOJITSUSLP/maptitudebox/src/App.vue
@@ -9,13 +9,8 @@
       style="width: 284px;"
     >
       <v-tabs icons-and-text v-model="vmodel_layertab">
-        <v-tab href="#capas">Capas<v-icon>mdi-view_list</v-icon></v-tab>
-        <v-tab href="#base">Mapas Base<v-icon>mdi-info</v-icon></v-tab>
-        <v-tab href="#base">Mapas Base<v-icon>mdi-info</v-icon></v-tab>
-        <v-tab href="#base">Mapas Base<v-icon>mdi-filter</v-icon></v-tab>
-        <v-tab href="#base">Mapas Base<v-icon>mdi-settings</v-icon></v-tab>
-        <v-tab href="#base">Mapas Base<v-icon>mdi-delete</v-icon></v-tab>
-
+        <v-tab href="#capas">Capas<v-icon>mdi-layers</v-icon></v-tab>
+        <v-tab href="#base">Mapas Base<v-icon>mdi-map</v-icon></v-tab>
       </v-tabs>
 
       <v-tabs-items v-model="vmodel_layertab">
@@ -75,13 +70,24 @@
                   <v-col pa-0>
                     <v-row justify="end">
                       <v-btn-toggle no-toggle>
-                        <v-btn color="blue-grey" style="height: 28px; width: 28px; min-width: 28px;">
+                        <v-btn
+                          color="blue-grey"
+                          style="height: 28px; width: 28px; min-width: 28px;"
+                        >
                           <v-icon color="grey lighten-5">mdi-chevron-up</v-icon>
                         </v-btn>
-                        <v-btn color="blue-grey" style="height: 28px; width: 28px; min-width: 28px;">
-                          <v-icon color="grey lighten-5">mdi-chevron-down</v-icon>
+                        <v-btn
+                          color="blue-grey"
+                          style="height: 28px; width: 28px; min-width: 28px;"
+                        >
+                          <v-icon color="grey lighten-5"
+                            >mdi-chevron-down</v-icon
+                          >
                         </v-btn>
-                        <v-btn color="red" style="height: 28px; width: 28px; min-width: 28px;">
+                        <v-btn
+                          color="red"
+                          style="height: 28px; width: 28px; min-width: 28px;"
+                        >
                           <v-icon color="grey lighten-5">mdi-delete</v-icon>
                         </v-btn>
                       </v-btn-toggle>
@@ -90,41 +96,113 @@
                 </v-row>
                 <v-row style="margin-top: -20px;">
                   <v-card class="mx-auto" max-width="245">
-                    <v-img class="black--text align-end text--secondary" height="137px" width="245px" src="https://api.parcelas-slp.maptitude.xyz/geoserver/SLP/wms?service=WMS&version=1.1.0&request=GetMap&layers=SLP%3Amunicipios&bbox=-1.1299319633648233E7%2C2472249.2933160546%2C-1.1214552619463304E7%2C2591032.4653501143&width=548&height=768&srs=EPSG%3A3857&format=image%2Fpng" aspect-ratio="1.7">
+                    <v-img
+                      class="black--text align-end text--secondary"
+                      height="137px"
+                      width="245px"
+                      src="https://api.parcelas-slp.maptitude.xyz/geoserver/SLP/wms?service=WMS&version=1.1.0&request=GetMap&layers=SLP%3Amunicipios&bbox=-1.1299319633648233E7%2C2472249.2933160546%2C-1.1214552619463304E7%2C2591032.4653501143&width=548&height=768&srs=EPSG%3A3857&format=image%2Fpng"
+                      aspect-ratio="1.7"
+                    >
                       <v-card-title>Municipios</v-card-title>
                     </v-img>
                     <v-card-text>
-                      <v-slider v-model="slider" thumb-label style="min-height: 20px; margin-left: 4px; margin-right: 4px;"></v-slider>
-                      <v-tabs v-model="vmodel_layertab">
-                        <v-tab href="#simbologia">Simbología<v-icon>mdi-layers</v-icon></v-tab>
-                        <v-tab href="#informacion">Información<v-icon>mdi-map</v-icon></v-tab>
-                      </v-tabs>
+                      <v-slider
+                        v-model="slider"
+                        thumb-label
+                        style="min-height: 20px; margin-left: 4px; margin-right: 4px;"
+                      ></v-slider>
                     </v-card-text>
+                    <v-tabs
+                      color="black"
+                      slider-size="3"
+                      v-model="vmodel_layericontabs"
+                    >
+                      <v-tab
+                        href="#legend"
+                        style="background-color: #9c27b0; min-width: 48px; max-width: 49px;"
+                      >
+                        <v-icon color="grey lighten-5">mdi-texture</v-icon>
+                      </v-tab>
+                      <v-tab
+                        href="#info"
+                        style="background-color: #00bcd4; min-width: 48px; max-width: 49px;"
+                      >
+                        <v-icon color="grey lighten-5">mdi-information</v-icon>
+                      </v-tab>
+                      <v-tab
+                        href="#filter"
+                        style="background-color: #cddc39; min-width: 48px; max-width: 49px;"
+                      >
+                        <v-icon color="grey lighten-5">mdi-filter</v-icon>
+                      </v-tab>
+                      <v-tab
+                        href="#settings"
+                        style="background-color: #607d8b; min-width: 48px; max-width: 49px;"
+                      >
+                        <v-icon color="grey lighten-5">mdi-settings</v-icon>
+                      </v-tab>
+                      <v-tab
+                        href="#download"
+                        style="background-color: #2196F3; min-width: 48px; max-width: 49px;"
+                      >
+                        <v-icon color="grey lighten-5"
+                          >mdi-cloud-download</v-icon
+                        >
+                      </v-tab>
+                    </v-tabs>
+                    <v-tabs-items v-model="vmodel_layericontabs">
+                      <v-tab-item value="legend">
+                        <v-card>
+                          <v-card-text>
+                            <v-img
+                              src="https://api.parcelas-slp.maptitude.xyz/geoserver/wms?request=GetLegendGraphic&version=1.1.1&format=image/png&width=30&HEIGHT=30&layer=SLP:municipios&legend_options=fontName:Arial;fontAntiAliasing:true;fontSize:16"
+                            />
+                          </v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                      <v-tab-item value="info">
+                        <v-card>
+                          <v-card-text>
+                            info
+                          </v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                      <v-tab-item value="filter">
+                        <v-card>
+                          <v-card-text>
+                            info
+                          </v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                      <v-tab-item value="settings">
+                        <v-card>
+                          <v-card-text>
+                            info
+                          </v-card-text>
+                        </v-card>
+                      </v-tab-item>
+                      <v-tab-item value="download">
+
+                            <v-list dense>
+                              <v-list-item-group v-model="items" color="primary">
+                                <v-list-item>
+                                  <v-list-item-content>
+                                    <v-list-item-title
+                                      v-text="items[0].text"
+                                    ></v-list-item-title>
+                                  </v-list-item-content>
+                                </v-list-item>
+                                <v-list-item>
+                                  Shapefile
+                                </v-list-item>
+                              </v-list-item-group>
+                            </v-list>
+                      </v-tab-item>
+                    </v-tabs-items>
                   </v-card>
                 </v-row>
-                <v-row align="center" justify="center">
-                  <v-btn-toggle>
-                    <v-btn color="purple">
-                      <v-icon color="grey lighten-5">mdi-texture</v-icon>
-                    </v-btn>
-                    <v-btn color="cyan">
-                      <v-icon color="grey lighten-5">mdi-information</v-icon>
-                    </v-btn>
-                    <v-btn color="lime">
-                      <v-icon color="grey lighten-5">mdi-filter</v-icon>
-                    </v-btn>
-                    <v-btn color="blue-grey">
-                      <v-icon color="grey lighten-5">mdi-settings</v-icon>
-                    </v-btn>
-                    <v-btn color="blue">
-                      <v-icon color="grey lighten-5">mdi-cloud-download</v-icon>
-                    </v-btn>
-                  </v-btn-toggle>
-                </v-row>
-                <v-row>
-                  <v-img width="245px" src="https://api.parcelas-slp.maptitude.xyz/geoserver/wms?request=GetLegendGraphic&version=1.0.0&format=image/png&width=30&HEIGHT=30&layer=SLP:municipios&legend_options=fontName:Arial;fontAntiAliasing:true;fontSize:12" />
-                </v-row>
-                <p> </p>
+
+                <p></p>
                 <v-divider bold></v-divider>
               </v-expansion-panel-content>
             </v-expansion-panel>
@@ -294,11 +372,16 @@ export default {
     }
   },
   data: () => ({
+    items: [
+      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Audience", icon: "mdi-account" },
+      { text: "Conversions", icon: "mdi-flag" }
+    ],
     municipios_src: {
       id: "municipios_source",
       type: "raster",
       tiles: [
-        "https://api.parcelas-slp.maptitude.xyz/geoserver/wms?&service=WMS&request=GetMap&layers=SLP%3Amunicipios&styles=&format=image%2Fpng8&transparent=true&version=1.1.1&width=256&height=256&srs=EPSG%3A3857&bbox={bbox-epsg-3857}",
+        "https://api.parcelas-slp.maptitude.xyz/geoserver/wms?&service=WMS&request=GetMap&layers=SLP%3Amunicipios&styles=&format=image%2Fpng8&transparent=true&version=1.1.1&width=256&height=256&srs=EPSG%3A3857&bbox={bbox-epsg-3857}"
       ],
       tileSize: 256
     },
@@ -337,8 +420,14 @@ export default {
     threeLine: false,
     vmodel_capasbase: "osm",
     vmodel_layertab: "osm",
+    vmodel_layericontabs: null,
     slider: 80
   }),
+  methods: {
+    testa(val) {
+      alert(val);
+    }
+  },
   created() {
     // We need to set mapbox-gl library here in order to use it in template
     this.mapbox = Mapbox;
